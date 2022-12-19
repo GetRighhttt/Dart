469292c12860fec2a2c0232af91591fcf5574135
Stefan Bayne <stefanbayne@gmail.com>
2022-12-19 14:58:59 -0500

    nulls.

---
+++ .idea/languagexamples/basics/nulltypes.dart
@@ -0,0 +1,18 @@
+/**
+ * Null in programming is basically when the variable doesn't have a reference
+ * in memory.
+ *
+ * Historically, returning a null value causes a lot of errors, and most older
+ * programming languages did not have nullable - able to be null data types
+ * available.
+ *
+ * Now, we see in kotlin, swift, and other languages that we can declare a
+ * variable to be nullable, and dart has recently adopted this approach to
+ * help avoid massive errors and apps crashing.
+ */
+
+void main() {
+  /*
+  In Dart, we cna declare anything as null.
+   */
+}
