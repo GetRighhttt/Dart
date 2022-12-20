c88ec9663bfc6295d92ae1b8a863bf9750420720
Stefan Bayne <stefanbayne@gmail.com>
2022-12-20 13:12:27 -0500

    reading user input.:

--- .idea/languagexamples/advanced/userinput.dart
+++ .idea/languagexamples/advanced/userinput.dart
@@ -1,3 +1,5 @@
+import 'dart:io'; // necessary for input and output streams
+
 /**
  * User input in mobile development is usually taken in via a text field or by
  * some form of input variable.
@@ -5,5 +7,19 @@
  * Here we will show how we can read in input from the console to the screen.
  */
 void main() {
+  print("Enter your name: ");
 
+  /**
+   * This is going to take in a string. It does not take in a variable.
+   */
+  var name = stdin.readLineSync();
+  print("Hello $name");
+
+  /**
+   * readlinesync is optional so when we declare the variable and define it as a
+   * string, we must put a question mark.
+   */
+  print("Enter your last name: ");
+  String? lastName = stdin.readLineSync();
+  print("Hello $name $lastName");
 }
