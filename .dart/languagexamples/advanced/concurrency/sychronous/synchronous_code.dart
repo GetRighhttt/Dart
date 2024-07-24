918764d80b9f788e447831cc1b1f7689e882622c
Stefan Bayne <stefanbayne@220022522s-MacBook-Pro.local>
2024-07-24 19:13:10 -0400

    feat: added sychronous example.

--- .dart/languagexamples/advanced/concurrency/sychronous/synchronous_code.dart
+++ .dart/languagexamples/advanced/concurrency/sychronous/synchronous_code.dart
@@ -1,3 +1,17 @@
 /*
-Example showing single-threaded code without asynchronous operations..
+Example showing single-threaded code without synchronous operations..
+
+Sychronous - each step happens in sequence or one after the other.
+Isolate - component in which all of the Dart code runs.
 */
+
+import 'dart:math';
+
+void main() {
+  print('1 - sqrt(4) -> ${sqrt(4)}');
+  print('2 - sqrt(8) -> ${sqrt(8)}');
+  print('3 - sqrt(16) -> ${sqrt(16)}');
+  Future.delayed(
+      const Duration(seconds: 5), () => print('4 - sqrt(25) -> ${sqrt(25)}'));
+  print('5 - sqrt(36) -> ${sqrt(36)}');
+}
