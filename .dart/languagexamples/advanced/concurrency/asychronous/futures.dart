52a4da6caa6fe74c5811ac1dd58ece83a9e14a27
Stefan Bayne <stefanbayne@220022522s-MacBook-Pro.local>
2024-07-24 19:40:20 -0400

    feat: isolates, microtasks, a little future info.

--- .dart/languagexamples/advanced/concurrency/asychronous/futures.dart
+++ .dart/languagexamples/advanced/concurrency/asychronous/futures.dart
@@ -2,5 +2,6 @@
  * Futures in Dart are one time returns from a remote source asynchronously.
  *
  * Asynchronous - done without being in sequence.
+ * Anything that takes an undertermined amount of time should be called asynchronously.
  */
 void main() async {}
--- .dart/languagexamples/advanced/concurrency/sychronous/synchronous_code.dart
+++ .dart/languagexamples/advanced/concurrency/sychronous/synchronous_code.dart
@@ -1,8 +1,12 @@
 /*
 Example showing single-threaded code without synchronous operations..
 
-Sychronous - each step happens in sequence or one after the other.
+Sychronous - each step happens in sequence or one after the other; code
+happens one by one after each other.
 Isolate - component in which specializes in running all of the Dart code runs.
+Event Loop processes events sychronously regardless if one event is asynchronous.
+Dart can handle Parallelism by creating multiple Isolates but it comes at a cost
+of using a ton of resources.
 */
 
 import 'dart:math';
@@ -13,12 +17,29 @@ void main() {
 
   // this fututre should happen in sequence beofre the '2' becasue
   // it was created first.
-  Future.delayed(
-      const Duration(seconds: 5), () => print('4 - sqrt(25) -> ${sqrt(25)}'));
+  Future.delayed(const Duration(seconds: 5),
+      () => print('4 - sqrt(25) -> ${sqrt(25)} - Delayed Future'));
 
   print('5 - sqrt(36) -> ${sqrt(36)}');
 
   // this should be the last method printed to the screen
-  Future.delayed(
-      const Duration(seconds: 5), () => print('2 - sqrt(8) -> ${sqrt(8)}'));
+  Future.delayed(const Duration(seconds: 5),
+      () => print('2 - sqrt(8) -> ${sqrt(8)} - Delayed Future'));
+
+// the Microtask should be computed before the other futures
+  print('Starting Future Calculation:');
+  Future(() {
+    print('A');
+    print('B');
+    print('C');
+    print('D');
+    Future(() => print('E'));
+    Future(() => print('F'));
+    Future(() => print('G'));
+    Future.microtask(() => print('H - Microtask'));
+    Future.microtask(() => print('H - Microtask'));
+    Future.microtask(() => print('H - Microtask'));
+    Future.microtask(() => print('H - Microtask'));
+    print('I');
+  });
 }
